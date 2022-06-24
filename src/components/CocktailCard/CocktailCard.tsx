cfa1c9f7b1e90ae4fc35a050e72233533cd31b74
Nikita Permogorcev <reqaro@gmail.com>
2022-06-24 22:08:38 +0300

    add cocktailCard component & add adaptive grid & some fix

---
+++ src/components/CocktailCard/CocktailCard.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './CocktailCard.scss'
+
+type Props = {
+    img: string
+    alcoholByVolume: string
+    name: string
+    description: string
+}
+
+const CocktailCard = ({img, alcoholByVolume, name, description}: Props) => {
+    return (
+        <article className={'cocktail-card'}>
+            <img className={'cocktail-card__img'} src={img}/>
+        </article>
+    );
+};
+
+export {CocktailCard};
